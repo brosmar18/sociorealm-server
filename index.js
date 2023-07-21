e34d3d2baf66dec08b6b84d592d73efcafa8a26e
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-20 21:28:08 -0500

    mongodb connected

--- index.js
+++ index.js
@@ -33,3 +33,14 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({storage});
+
+/* MONGOOSE SETUP */
+
+const PORT = process.env.port || 6001;
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true, 
+}).then(() => {
+    app.listen(PORT, () => console.log(`Server PORT: ${PORT}`));
+})
+.catch((error) => console.log(`${error} did not connect`));
