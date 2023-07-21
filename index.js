a0e717afd2312e23c9503e2dc86ab15173ee8791
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-20 21:42:11 -0500

    bug fixed

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import multer from 'multer';
 import helmet from 'helmet';
 import morgan from 'morgan';
 import path from 'path';
+import cors from 'cors';
 import { fileURLToPath } from 'url';
 
 /* Configurations */
@@ -15,7 +16,7 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(helmet());
-app.use(helmet.crossOriginOpenerPolicy({policy: "cross-origin"}));
+app.use(helmet.crossOriginOpenerPolicy({policy: "same-origin"}));
 app.use(morgan("common"));
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
