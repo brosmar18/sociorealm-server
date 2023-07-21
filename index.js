d391f136b00176529fba7061756f065a60c46071
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-20 19:18:14 -0500

    File storage setup public/assets

--- index.js
+++ index.js
@@ -21,3 +21,13 @@ app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
+
+/* File Storage */
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "public/assets");
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    }
+});
