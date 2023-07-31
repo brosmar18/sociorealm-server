8409d9f2e1c4b0c993ea1c6be7ce2f992ec14f8d
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-31 08:12:40 -0500

    new file added, userRoutes import setup

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ import path from 'path';
 import cors from 'cors';
 import { fileURLToPath } from 'url';
 import authRoutes from "./routes/auth.js"; 
+import userRoutes from './routes/users.js';
 import { register } from './controllers/auth.js';
 
 /* Configurations */
@@ -42,6 +43,7 @@ app.post("/auth/register", upload.single("picture", register));
 
 /* ROUTES */
 app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
 
 
 /* MONGOOSE SETUP */
